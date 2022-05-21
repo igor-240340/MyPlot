e2094cb7a062cc3e4872786059ff7a56519e0cf9
igor <igor.240340@gmail.com>
2022-05-21 16:33:22 +0700

    Polish

--- MyPlot/MyPlot.cpp
+++ MyPlot/MyPlot.cpp
@@ -1,25 +1,66 @@
 #include "pch.h"
+
+#include <thread>
+#include <queue>
+#include <mutex>
+
 #include "MyPlot.h"
 #include "matplotlibcpp.h"
-#include <thread>
-#include "processthreadsapi.h"
-#include <mutex>
-#include <queue>
 
 namespace plt = matplotlibcpp;
 
-std::thread t;
+std::queue<std::function<void()>> plotFuncsQueue;	// Accumulates plot-functions that must be called in uiThread
+std::mutex plotFuncsQueueMutex;
 
-std::vector<double> x = { 0.0f };
-std::vector<double> y = { 0.0f };
+// We need a separate thread so that we can interact with matplotlib UI even when the main thread is paused from Unity
+std::thread uiThread = std::thread(uiLoop);
 
-std::function<void()> callMeLater;
-std::queue<std::function<void()>> cachedFuncs;
+bool plot(const float* x, const float* y, int xySize, const KeywordValue* kw, int kwSize) {
+	std::vector<double> xVector = floatArrayToVector(x, xySize);
+	std::vector<double> yVector = floatArrayToVector(y, xySize);
 
-DWORD dwThreadId;
-HANDLE hThread;
+	std::map<std::string, std::string> kwMap = kwToKwMap(kw, kwSize);
 
-std::mutex cachedFuncsMutex;
+	auto f = [xVector, yVector, kwMap]() {
+		std::cout << "plt::plot()\n";
+		plt::plot(xVector, yVector, kwMap);
+	};
+	plotFuncsQueue.push(f);
+
+	return true;
+}
+
+void clf() {
+	plotFuncsQueueMutex.lock();
+
+	// If UI-loop don't handle some plot-functions by the next drawing iteration, 
+	// we just discard them to avoid buffer growing and, hence, wating for execution all old functions
+	// when main thread is paused from Unity - when we pause a game from Unity we need to see only last drawing.
+	std::queue<std::function<void()>> empty;
+	std::swap(plotFuncsQueue, empty);
+
+	plotFuncsQueue.push([]() {
+		std::cout << "plt::clf()\n";
+		plt::clf();
+		});
+
+	plotFuncsQueueMutex.unlock();
+}
+
+bool scatter(const float* x, const float* y, int xySize, const KeywordValue* kw, int kwSize, const float s) {
+	std::vector<double> xVector = floatArrayToVector(x, xySize);
+	std::vector<double> yVector = floatArrayToVector(y, xySize);
+
+	std::map<std::string, std::string> kwMap = kwToKwMap(kw, kwSize);
+
+	auto f = [xVector, yVector, s, kwMap]() {
+		std::cout << "plt::scatter()\n";
+		plt::scatter(xVector, yVector, s, kwMap);
+	};
+	plotFuncsQueue.push(f);
+	
+	return true;
+}
 
 std::vector<double> floatArrayToVector(const float* arr, int size) {
 	std::vector<double> v;
@@ -36,89 +77,18 @@ std::map<std::string, std::string> kwToKwMap(const KeywordValue* kw, int kwSize)
 	return kwMap;
 }
 
-bool plot(const float* x, const float* y, int xySize, const KeywordValue* kw, int kwSize) {
-	std::vector<double> xVector = floatArrayToVector(x, xySize);
-	std::vector<double> yVector = floatArrayToVector(y, xySize);
-
-	std::map<std::string, std::string> kwMap = kwToKwMap(kw, kwSize);
-
-	//std::lock_guard<std::mutex> guard(cacheMutex);
-	//if (cachedFuncsMutex.try_lock()) {
-		auto cachedCall = [xVector, yVector, kwMap]() {
-			std::cout << "plt::plot()\n";
-			plt::plot(xVector, yVector, kwMap);
-		};
-		cachedFuncs.push(cachedCall);
-		//cachedFuncsMutex.unlock();
-	//}
-
-	return true;
-}
-
-void show(const bool block) {
-	plt::show(block);
-}
-
-void clf() {
-	cachedFuncsMutex.lock();
-	
-	std::queue<std::function<void()>> empty;
-	std::swap(cachedFuncs, empty);
-
-	cachedFuncs.push([]() {
-		std::cout << "plt::clf()\n";
-		plt::clf();
-	});
-
-	cachedFuncsMutex.unlock();
-}
-
-bool scatter(const float* x, const float* y, int xySize, const KeywordValue* kw, int kwSize, const float s) {
-	std::vector<double> xVector = floatArrayToVector(x, xySize);
-	std::vector<double> yVector = floatArrayToVector(y, xySize);
-
-	std::map<std::string, std::string> kwMap = kwToKwMap(kw, kwSize);
-
-	std::cout << "scatter()\n";
-	//std::lock_guard<std::mutex> guard(cachedFuncsMutex);
-	//if (cachedFuncsMutex.try_lock()) {
-		auto cachedCall = [xVector, yVector, s, kwMap]() {
-			std::cout << "plt::scatter()\n";
-			plt::scatter(xVector, yVector, s, kwMap);
-		};
-		cachedFuncs.push(cachedCall);
-		//cachedFuncsMutex.unlock();
-	//}
-
-	return true;
-	//plt::scatter(xVector, yVector, s, kwMap);
-}
-
-void pause(float interval) {
-	plt::pause(interval);
-}
-
-void callCached() {
+void uiLoop() {
 	while (1) {
-		//std::lock_guard<std::mutex> guard(cachedFuncsMutex);
-		//if (cachedFuncsMutex.try_lock()) {
-		if (!cachedFuncs.empty()) {
-			if (cachedFuncsMutex.try_lock()) {
-				while (!cachedFuncs.empty()) {
-					auto f = cachedFuncs.front();
-					f();
-					cachedFuncs.pop();
-				};
-				cachedFuncsMutex.unlock();
+		// Lock mutex to prevent queue wiping from the main thread and pop() call on an empty queue (that cause segfault)
+		if (!plotFuncsQueue.empty() && plotFuncsQueueMutex.try_lock()) {
+			while (!plotFuncsQueue.empty()) {
+				auto f = plotFuncsQueue.front();
+				f();
+				plotFuncsQueue.pop();
 			}
+			plotFuncsQueueMutex.unlock();
 		}
-			//cachedFuncsMutex.unlock();
-		//}
 
-		plt::pause(0.01);
+		plt::pause(0.01); // Draw
 	}
-}
-
-void runInThread() {
-	t = std::thread(callCached);
 }
--- MyPlot/MyPlot.h
+++ MyPlot/MyPlot.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <map>
+
 #define DLL_EXPORT extern "C" __declspec(dllexport)
 
 struct KeywordValue {
@@ -8,11 +10,9 @@ struct KeywordValue {
 };
 
 DLL_EXPORT bool plot(const float* x, const float* y, int xySize, const KeywordValue* kw, int kwSize);
-DLL_EXPORT void show(const bool block = true);
 DLL_EXPORT bool scatter(const float* x, const float* y, int xySize, const KeywordValue* kw, int kwSize, const float s = 1.0f);
-DLL_EXPORT void pause(float interval);
 DLL_EXPORT void clf();
 
-DLL_EXPORT void test();
-DLL_EXPORT void runInThread();
-DLL_EXPORT void update(double a, double b);
+std::vector<double> floatArrayToVector(const float* arr, int size);
+std::map<std::string, std::string> kwToKwMap(const KeywordValue* kw, int kwSize);
+void uiLoop();
